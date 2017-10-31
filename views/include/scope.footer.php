e7b8453b357c2ed25f519fa03894a77c9c1e3f55
evelinFrontend <ginatha97@gmail.com>
2017-10-31 14:15:25 -0500

    conecte el parslay

--- views/include/scope.footer.php
+++ views/include/scope.footer.php
@@ -3,8 +3,10 @@
         	</div>
         </section>
  	 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <script src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-         <script src="views/assets/js/main.js"></script>
+   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+   <script src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+   <script src="view/assets/lib/parsley/parsley.min.js"></script>
+   <script src="view/assets/lib/parsley/es.js"></script>
+   <script src="views/assets/js/main.js"></script>
     </body>
 </html>
