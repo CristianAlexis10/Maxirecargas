45ada075c5a9a90c5af1c30a9ae1fd918d07a678
evelinFrontend <ginatha97@gmail.com>
2017-11-01 23:28:06 -0500

    input del contacto

--- views/assets/js/main-user.js
+++ views/assets/js/main-user.js
@@ -41,5 +41,12 @@ signin.onclick = function() {
 };
 
 $(".input-contact").focus(function(){
-    $(this).parent().addClass("color-label mover-label");
+  $(this).parent().addClass("color-label mover-label");
+});
+
+$(".input-contact").focusout(function(){
+  if($(this).val() === "")
+    $(this).parent().removeClass("mover-label");
+    $(this).parent().removeClass("color-label");
+
 });
