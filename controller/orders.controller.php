782dd55746e8b9d771be94a4b45668f13f8a986d
Cristisn Lopera <cristian1020011@gmail.com>
2017-11-07 16:23:30 -0500

    permisos

--- controller/orders.controller.php
+++ controller/orders.controller.php
@@ -5,14 +5,22 @@
 	 		$this->master = new MasterModel;
 	 	}
 		function main(){
-			if (isset($_SESSION['CUSTOMER']['ROL']) && $_SESSION['CUSTOMER']['ROL']==3) {
-				// require_once "views/include/scope.header.php";
-				require_once "views/modules/customer/orders/orders.php";
-				// require_once "views/include/scope.footer.php";
-			}elseif (isset($_SESSION['CUSTOMER']['ROL'])!=3) {
-				require_once "views/include/scope.header.php";
-				require_once "views/modules/admin/orders/index.php";
-				require_once "views/include/scope.footer.php";
+			if (isset($_SESSION['CUSTOMER']['ROL'])) {
+				foreach ($_SESSION['CUSTOMER']['PERMITS'] as $row) {
+					if ($row['enlace']=='clientes') {
+						$access = true;
+					}
+				}
+				if (isset($access)) {
+					require_once "views/include/scope.header.php";
+					require_once "views/modules/admin/orders/index.php";
+					require_once "views/include/scope.footer.php";
+				}else{
+					// require_once "views/include/scope.header.php";
+					require_once "views/modules/customer/orders/orders.php";
+					// require_once "views/include/scope.footer.php";
+
+				}
 			}else{
 				require_once "views/include/user/scope.header.php";
 				require_once "views/modules/user/orders/index.php";
