5e8d6ad48fe72146d9b813e6bf9d79f19be266ae
Cristisn Lopera <cristian1020011@gmail.com>
2017-11-07 14:26:40 -0500

    htacess

--- controller/views.controller.php
+++ controller/views.controller.php
@@ -8,6 +8,13 @@
 				require_once "views/modules/landing.html";
 			}
 		}
+	 	function newUser(){
+			if (isset($_SESSION['CUSTOMER']['ROL'])) {
+					require_once "views/modules/customer/index.php";
+			}else{
+				require_once "views/modules/landing.html";
+			}
+		}
 
 	}
 ?>
