7b4372e810caf3824c9c22960eebae36c4cbed32
Cristisn Lopera <cristian1020011@gmail.com>
2017-11-07 16:16:04 -0500

    2

--- controller/orders.controller.php
+++ controller/orders.controller.php
@@ -5,7 +5,11 @@
 	 		$this->master = new MasterModel;
 	 	}
 		function main(){
-			if (isset($_SESSION['CUSTOMER']['ROL'])) {
+			if (isset($_SESSION['CUSTOMER']['ROL']) && $_SESSION['CUSTOMER']['ROL']==3) {
+				// require_once "views/include/scope.header.php";
+				require_once "views/modules/customer/orders/orders.php";
+				// require_once "views/include/scope.footer.php";
+			}elseif (isset($_SESSION['CUSTOMER']['ROL'])!=3) {
 				require_once "views/include/scope.header.php";
 				require_once "views/modules/admin/orders/index.php";
 				require_once "views/include/scope.footer.php";
