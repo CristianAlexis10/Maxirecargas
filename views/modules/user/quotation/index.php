dd08fffd260a3d3623a923d04714c9c44f65c1ac
Estefanía Tapias <etapias41@misena.edu.co>
2017-11-09 11:30:01 -0500

    index-quotation

--- views/modules/user/quotation/index.php
+++ views/modules/user/quotation/index.php
@@ -1,1 +1,63 @@
-usuario
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title> Maxirecargas</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="views/assets/image/logo.png">
+    <link rel="stylesheet" href="views/assets/css/main-user.css">
+  </head>
+  <body>
+    <div class="main--container--quotation">
+      <div class="left--part--quotation">
+        <span><i class="fa fa-bars" aria-hidden="true" id="menu"></i></span>
+        <div class="title--main--quotation">
+          <p>Cotizaciones</p>
+          <div class="triangle">
+            <div class="square--quotation"><p>Crea tu cotizacion</p></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="rigth--part--quotation">
+      <div class="container--form--quotation">
+        <form class="form--quotation" action="" method="post">
+        <div class="input--group--quotation">
+          <label for="request" class="label--quotation">Tipo de solicitud</label>
+          <input type="text" name="" value="" id="request" class="input--quotation">
+        </div>
+
+        <div class="input--group--quotation">
+          <label for="reference" class="label--quotation">Referencia del producto</label>
+          <input type="text" name="" value="" id="reference" class="input--quotation">
+        </div>
+
+        <div class="input--group--quotation">
+          <label for="quantity" class="label--quotation">Cantidad</label>
+          <input type="text" name="" value="" id="quantity" class="input--quotation">
+        </div>
+
+        <div class="input--group--quotation">
+          <label for="description" class="label--quotation">Descripcion del problema</label>
+          <input type="text" name="" value="" id="description" class="input--quotation">
+        </div>
+
+        <button type="button" name="button" class="buttom--quotation">Cotizar</button>
+
+      </form>
+      </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="views/assets/lib/animate/animatescroll.js"> </script>
+    <script src="views/assets/lib/parsley/parsley.min.js"></script>
+    <script src="views/assets/lib/parsley/es.js"></script>
+    <script type="text/javascript" src="views/assets/js/main-user.js"></script>
+    <script type="text/javascript" src="views/assets/js/auth.js"></script>
+    <script type="text/javascript" src="views/assets/js/menu.js"></script>
+
+  </body>
+</html>
