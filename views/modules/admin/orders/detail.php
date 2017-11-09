06a00990894dccae7cbd7bb3b96d707f3dd6bfdb
evelinherrera <eyherrera45@misena.edu.co>
2017-11-08 21:49:28 -0500

    hace algo

--- views/modules/admin/orders/detail.php
+++ views/modules/admin/orders/detail.php
@@ -2,23 +2,23 @@
 	<div class="wrap--info">
 		<div class="detail">
 			<p class="item--detail">Codigo del pedido:</p>
-			<p class="data--detail">xxxxxxxxxxxxxx> </p>
+			<p class="data--detail">12354 </p>
 		</div>
 		<div class="detail">
 			<p class="item--detail">Cliente:</p>
-			<p class="data--detail">xxxxxxxxxxxxxx> </p>
+			<p class="data--detail">Cristian Lopera </p>
 		</div>
 		<div class="detail">
 			<p class="item--detail">Fecha de entrega:</p>
-			<p class="data--detail">xxxxxxxxxxxxxx> </p>
+			<p class="data--detail">9 sep 2017 </p>
 		</div>
 		<div class="detail">
 			<p class="item--detail">Dirección:</p>
-			<p class="data--detail">xxxxxxxxxxxxxx> </p>
+			<p class="data--detail">calle 5 sur #456-34 </p>
 		</div>
 		<div class="detail">
 			<p class="item--detail">Telefono:</p>
-			<p class="data--detail">xxxxxxxxxxxxxx> </p>
+			<p class="data--detail">5824852 </p>
 		</div>
 			<li class="item mapa"> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d179280.30874201766!2d12.241636541580144!3d45.404200754014774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477eb1daf1d63d89%3A0x7ba3c6f0bd92102f!2sVenecia%2C+Italia!5e0!3m2!1ses!2sco!4v1507838940585" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></li>
 		</ul>
