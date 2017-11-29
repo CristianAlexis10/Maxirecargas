f2631bf5053d51be37dda115e5c8e1be52c1a98a
evelinFrontend <ginatha97@gmail.com>
2017-11-29 17:26:23 -0500

    grafica

---
+++ views/assets/js/widgets.js
@@ -0,0 +1,39 @@
+var ctx = document.getElementById("first--pie");
+var chartPie = new Chart(ctx,{
+    type:'pie',
+    data: {
+        datasets: [{
+            data: [12,34,56,12,34],
+            backgroundColor: [
+                window.chartColors.pink,
+                window.chartColors.red,
+                window.chartColors.purple,
+                window.chartColors.black,
+                window.chartColors.yellow,
+            ],
+            // label: 'Dataset 1'
+        }],
+        labels:["pink","red","purple","black","yellow"]},
+        option:{
+          responsive: true
+        }});
+
+var ctx2 = document.getElementById("second--pie");
+  var chartPie2 = new Chart(ctx2,{
+            type:'pie',
+            data: {
+                datasets: [{
+                    data: [12,34,56,12,34],
+                    backgroundColor: [
+                        window.chartColors.pink,
+                        window.chartColors.red,
+                        window.chartColors.purple,
+                        window.chartColors.black,
+                        window.chartColors.yellow,
+                    ],
+                    label: 'que mierda es eso'
+                }],
+                labels:["pink","red","purple","black","yellow"]},
+                option:{
+                  responsive: true
+                }});
--- views/modules/admin/dashboard.php
+++ views/modules/admin/dashboard.php
@@ -1,14 +1,9 @@
-<div class="modules">
-	<div class="title">
-		<p>ESTADISITICAS</p>
-	</div>
-</div>
-
-
-</article>
-</div>
-</section>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.js"></script>
-<script src="views/assets/lib/chart/utils.js"></script>
-</body>
-</html>
+		<div class="modules">
+			<div class="title">
+				<p>ESTADISITICAS</p>
+			</div>
+		</div>
+		<div class="container--charts">
+			<canvas id="first--pie"></canvas>
+			<canvas id="second--pie"></canvas>
+		</div>
