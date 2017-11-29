c086556a83a38fca5e47d2b6601c5a759d69710a
evelinFrontend <ginatha97@gmail.com>
2017-11-29 13:21:27 -0500

    cosa

--- views/modules/admin/dashboard.php
+++ views/modules/admin/dashboard.php
@@ -3,5 +3,12 @@
 		<p>ESTADISITICAS</p>
 	</div>
 </div>
+
+
+</article>
+</div>
+</section>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.js"></script>
 <script src="views/assets/lib/chart/utils.js"></script>
+</body>
+</html>
