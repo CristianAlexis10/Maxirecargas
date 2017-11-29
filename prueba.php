1db13e816f41a93074f7f32c4efdb352545b22bc
evelinFrontend <ginatha97@gmail.com>
2017-11-29 13:05:46 -0500

    nada

---
+++ prueba.php
@@ -0,0 +1,10 @@
+<?php
+
+//Aca vamos a retornar un json que haria lopera
+
+$data = array(1, 20, 13, 15, 12, 13);
+echo json_encode($data);
+
+
+
+ ?>
--- views/modules/admin/dashboard.php
+++ views/modules/admin/dashboard.php
@@ -1,3 +1,7 @@
 <div class="modules">
-	
+	<div class="title">
+		<p>ESTADISITICAS</p>
+	</div>
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.js"></script>
+<script src="views/assets/lib/chart/utils.js"></script>
