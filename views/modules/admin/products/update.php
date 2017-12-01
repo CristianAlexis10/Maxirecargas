b7d67ae9d9ce52d9eafa873429a1dbcb04533389
Cristisn Lopera <cristian1020011@gmail.com>
2017-12-01 17:18:51 -0500

    a

--- views/modules/admin/products/update.php
+++ views/modules/admin/products/update.php
@@ -1,24 +1,77 @@
 <?php
 $result =$this->readBy(base64_decode($_GET['data']));
-$_SESSION['category_update']=base64_decode($_GET['data']);
+$_SESSION['product_update']=base64_decode($_GET['data']);
 ?>
 <div class="modules services">
 	<div class="title">
 		<p>MODIFICAR PRODUCTO</p>
 	</div>
 
-		    	<form class="frmServices" action="guardar-modificacion-categoria" method="post">
-		    		 <div class="form-group">
-			                <label for="nombre" class="required">Nombre:</label>
-			                <input type="text" name="data[]" id="nombre" value="<?php echo $result['mar_nombre']?>" required>
-			            </div>
-			             <div class="form-group">
-			                <label for="des" class="required">Descripción:</label>
-			                <textarea name="data[]" id="des"><?php echo $result['mar_descripcion']?></textarea>
-			            </div>
 
-			            <div class="form-group">
-			                <button class="btn">Actualizar Categoria</button>
-			            </div>
-		    	</form>
+<div class="form--left">
+
+	<form  id="frmNerProduct">
+		  <div class="form-group">
+				<label for="marca" class="required ">Marca:</label>
+			   <select name="data[]" id="marca" class="data-new-pro input" required >
+				<?php foreach($this->master->selectAll("marca") AS $row) {?>
+					<option value="<?php echo $row['mar_codigo']?>"><?php echo $row['mar_nombre'] ?></option>
+				<?php } ?>
+				<option value="newMark">Otra</option>
+			   </select>
+			</div>
+			<div class="form-group">
+				<label for="categoria" class="required">Categoria:</label>
+			   <select  id="categoria" class="data-new-pro input " required>
+				<?php foreach($this->master->selectAll("tipo_producto") AS $row) {?>
+					<option value="<?php echo $row['tip_pro_codigo']?>"><?php echo $row['tip_pro_nombre'] ?></option>
+				<?php } ?>
+				<option value="newCategory">Otra</option>
+			   </select>
+			</div>
+		 <div class="form-group">
+				<label for="rf" class="label">Referencia:</label>
+				<input type="text" name="data[]" id="rf" class="input data-new-pro" required>
+			</div>
+
+			<?php
+			foreach ($this->master->selectAll("tipo_servicio") AS $row) {?>
+		  <label><?php echo $row['tip_ser_nombre'] ?>
+			<input type="checkbox"  name="ch-tip-ser" value="<?php echo $row['Tip_ser_cod']?>"  >
+		  </label>
+
+			<?php }  ?>
+			 <div class="form-group">
+				<label for="caracteristica" class="label">Caracteristicas:</label>
+								<textarea name="data[]" id="caracteristica" class="input data-new-pro"></textarea>
+
+			</div>
+			<div class="form-group">
+				<input type="taxt" id="img" class="data-new-pro" value="icn-maxi.png">
+			</div>
+
+
+		</div>
+					<div class="form--rigth rigth--product">
+						<div class="form-group Cambiar--img">
+						 <div id="wrap-result"><img src="views/assets/image/icn-maxi.png" ></div>
+						 <span class="" id="cropp-img">Cambiar foto</span>
+					 </div>
+				<div class="form-group">
+					<button class="btn" >Modificar</button>
+				</div>
+					</div>
+	</form>
+
+
+
+	<div class="img-product">
+		<div class="newMark--img">
+			<div id="uploadImage">
+				<div id="wrap-upload" style="width:350px"></div>
+				<input type="file" id="upload">
+				<button class="btn btn-success upload-result">Recortar Imagen</button>
+			  </div>
+		</div>
+	</div>
 </div>
