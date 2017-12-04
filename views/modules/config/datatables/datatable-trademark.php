dc07a51a5e848cfc3c37302d79b881abdb2964d7
CristianAlexis10 <cristian1020011@gmail.com>
2017-12-03 16:51:39 -0800

    f

--- views/modules/config/datatables/datatable-trademark.php
+++ views/modules/config/datatables/datatable-trademark.php
@@ -12,12 +12,19 @@
                          <td><?php echo $row['mar_nombre'];?></td>
                             <td><?php echo $row['mar_descripcion'];?></td>
                            <td>
+                            <?php 
+                             $modulo = 'productos';
+                            $crud = permisos($modulo,$permit);      
+                            if ($crud[2]==true) {?>
                             <a href="modificar-marca-<?php echo rtrim(strtr(base64_encode($row['mar_codigo']), '+/', '-_'), '=');?>" ><i class="fa fa-pencil-square-o"></i></a>
+                            <?php }  if ($crud[3]==true) {?>
                             <a href="#" onclick="return confirmDeleteMark(
                             <?php
                          echo $row['mar_codigo'];
                             ?>)"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
                            </td>
+                           
+                           <?php } ?>
                       </tr>
 
                 <?php	} ?>
