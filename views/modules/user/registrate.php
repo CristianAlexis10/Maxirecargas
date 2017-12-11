9019445d36da4588422cfa1a274202be469a5006
evelinFrontend <ginatha97@gmail.com>
2017-12-10 23:28:03 -0500

    no lo hagas

--- views/modules/user/registrate.php
+++ views/modules/user/registrate.php
@@ -67,13 +67,13 @@
                     <select class="dataCl input"  id="cuidad" required> </select>
                   </div>
                   <div class="form-group">
-<<<<<<< HEAD
+
                     <label for="fecha_naci" class="required select">Fecha de Nacimiento:</label>
                     <input type="date" name="data"  id="fecha_naci" class="input dataCl" max="2005-01-01" min="1950-01-01" required>
-=======
-                    <label for="dir" class="required">Dirección:</label>
+                  </div>
+                  <div class="form-group">
+                    <label for="dir" class="required label">Dirección:</label>
                     <input type="text" name="data"  id="dir" class="input dataCl"  required>
->>>>>>> 6376187954053180af514a8860da991147a6a1f1
                   </div>
                   <button type="button"  id="irAtras">atras</button>
                   <button type="button" id="normalIrParte3">siguiente</button>
