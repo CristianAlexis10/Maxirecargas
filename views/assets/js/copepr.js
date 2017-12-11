449ea1a073234456fbb162909a5e4bca9a2362a3
evelinFrontend <ginatha97@gmail.com>
2017-12-11 12:34:15 -0500

    dsvcs

--- views/assets/js/copepr.js
+++ views/assets/js/copepr.js
@@ -1,27 +1,29 @@
-// $(".inputuser").focus(function(){
-//   $(this).parent().addClass("color-labeluser mover-labeluser");
-// });
-//
-// $(".inputuser").focusout(function(){
-//   if($(this).val() === "")
-//     $(this).parent().removeClass("mover-labeluser");
-//     $(this).parent().removeClass("color-labeluser");
-// });
-// $(".inputuser2").focus(function(){
-//   $(this).parent().addClass("color-labeluser2 mover-labeluser2");
-// });
-//
-// $(".inputuser2").focusout(function(){
-//   if($(this).val() === "")
-//     $(this).parent().removeClass("mover-labeluser2");
-//     $(this).parent().removeClass("color-labeluser2");
-// });
-//
-// var CloseModal = document.getElementById('closeConfir');
-// var modal = document.getElementById('modalConfir');
-// CloseModal.onclick= function(){
-//   modal.style.display = "none";
-// };
+$(".inputuser").focus(function(){
+  $(this).parent().addClass("color-labeluser mover-labeluser");
+});
+
+$(".inputuser").focusout(function(){
+  if($(this).val() === "")
+    $(this).parent().removeClass("mover-labeluser");
+    $(this).parent().removeClass("color-labeluser");
+});
+$(".inputuser2").focus(function(){
+  $(this).parent().addClass("color-labeluser2 mover-labeluser2");
+});
+
+$(".inputuser2").focusout(function(){
+  if($(this).val() === "")
+    $(this).parent().removeClass("mover-labeluser2");
+    $(this).parent().removeClass("color-labeluser2");
+});
+if (document.getElementById('closeConfir')) {
+  var CloseModal = document.getElementById('closeConfir');
+  var modal = document.getElementById('modalConfir');
+  CloseModal.onclick= function(){
+    modal.style.display = "none";
+  };
+
+}
 
 
 //views order mover
