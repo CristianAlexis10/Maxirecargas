34889054143e2cf74f7133e330ffe394fd99ba30
evelinFrontend <ginatha97@gmail.com>
2018-02-13 16:16:49 -0500

    hello it's me

--- views/modules/customer/activateAccount.php
+++ views/modules/customer/activateAccount.php
@@ -1,4 +1,22 @@
-<?php
-
-?>
-<a href="activarCuenta-<?php echo $_GET['token'] ?>">Click aqui para activar tu cuenta</a>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ultimo paso</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="shortcut icon" href="views/assets/image/icn-maxi.png">
+    <link rel="stylesheet" href="views/assets/css/main-user.css">
+    <link rel="stylesheet" href="views/assets/css/responsive.css">
+  </head>
+  <body>
+    <div class="container--activar">
+      <div class="content--activar">
+        <img src="views/assets/image/logo.png">
+        <h1>ultimo paso</h1>
+        <a href="activarCuenta-<?php echo $_GET['token'] ?>">Click aqui para activar tu cuenta</a>
+      </div>
+      <div class="figura1"></div>
+      <div class="figura2"></div>
+    </div>
+  </body>
+</html>
