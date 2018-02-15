b811d851c91454eafde857903a037bc5f28e47b6
evelinFrontend <ginatha97@gmail.com>
2018-02-15 15:06:26 -0500

    error404 listo

--- views/modules/config/errors/error404.php
+++ views/modules/config/errors/error404.php
@@ -1,1 +1,21 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title>Maxirecargas</title>
+    <link rel="shortcut icon" href="views/assets/image/logo.png">
+    <link rel="stylesheet" href="views/assets/css/error.css">
+  </head>
+  <body>
+    <div class="container--error">
+      <img src="views/assets/image/logo.png">
+      <div class="error">
+        <h1>Error 404</h1>
+        <p>La Pagina que buscas no Existe o no se Encuentra Disponible!</p>
+      </div>
+      <div class="figura1"></div>
+      <div class="figura2"></div>
+    </div>
+  </body>
+</html>
