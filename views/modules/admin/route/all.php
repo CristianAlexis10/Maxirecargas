d5d607eb20b5dcda0af0d8c945a4eeb358b9cb32
CristianAlexis10 <cristian1020011@gmail.com>
2018-02-26 12:34:31 -0500

    asd

--- views/modules/admin/route/all.php
+++ views/modules/admin/route/all.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <?php
-=======
- <?php
->>>>>>> e78f349609bdfe5649d4bbe2f4a46b6dfe262242
 if ($crud[1]==true) {
 $count = $this->master->ContarRutasPorUsuario(base64_decode($_GET['data']),date('Y-m-d'));
 $data = $this->master->verDetalleRuta(date('Y-m-d'),base64_decode($_GET['data']));
@@ -10,172 +6,168 @@ $_SESSION['mapRouteToday']= $data;
 if($data!=array()){
 ?>
 <div class="mudules orders detail" id="detail-reload">
-	<div class="wrap--info">
-		<div class="detail">
-			<p class="item--detail">Nombre Encargado:</p>
-			<p class="data--detail"><?php echo $data[0]['usu_primer_nombre']." ".$data[0]['usu_primer_apellido'];?>  </p>
-		</div>
-		<div class="detail">
-			<p class="item--detail">Correo:</p>
-			<p class="data--detail"><?php echo $data[0]['usu_correo']?></p>
-		</div>
-		<div class="detail">
-			<p class="item--detail">Numero de Celular:</p>
-			<p class="data--detail"><?php echo $data[0]['usu_celular'];?> </p>
-		</div>
-		<div class="detail">
-			<p class="item--detail">Total de visitas:</p>
-			<p class="data--detail"><?php echo $count['total'];?> </p>
-		</div>
-		<div class="detail">
-			<p class="item--detail">Total de visitas Terminadas:</p>
-			<p class="data--detail"><?php  echo $this->master->contarPedidosTerminadosBy(base64_decode($_GET['data']))['total'];?> </p>
-		</div>
-		<div class="detail">
-			<p class="item--detail">Total de visitas Pendientes:</p>
-			<p class="data--detail"><?php  echo $this->master->contarPedidosPendientesBy(base64_decode($_GET['data']),date('Y-m-d'))['total'];?> </p>
-		</div>
-		<div class="detail">
-			<p class="item--detail">Total de visitas Canceladas:</p>
-			<p class="data--detail"><?php  echo $this->master->contarPedidosCanceladasBy(base64_decode($_GET['data']))['total'];?> </p>
-		</div>
-		<div class="detail">
-			<a href="#"  class="contact--customer" id="<?php echo $data[0]['usu_codigo']; ?>">Contactar encargado</a>
-		</div>
+ <div class="wrap--info">
+   <div class="detail">
+     <p class="item--detail">Nombre Encargado:</p>
+     <p class="data--detail"><?php echo $data[0]['usu_primer_nombre']." ".$data[0]['usu_primer_apellido'];?>  </p>
+   </div>
+   <div class="detail">
+     <p class="item--detail">Correo:</p>
+     <p class="data--detail"><?php echo $data[0]['usu_correo']?></p>
+   </div>
+   <div class="detail">
+     <p class="item--detail">Numero de Celular:</p>
+     <p class="data--detail"><?php echo $data[0]['usu_celular'];?> </p>
+   </div>
+   <div class="detail">
+     <p class="item--detail">Total de visitas:</p>
+     <p class="data--detail"><?php echo $count['total'];?> </p>
+   </div>
+   <div class="detail">
+     <p class="item--detail">Total de visitas Terminadas:</p>
+     <p class="data--detail"><?php  echo $this->master->contarPedidosTerminadosBy(base64_decode($_GET['data']))['total'];?> </p>
+   </div>
+   <div class="detail">
+     <p class="item--detail">Total de visitas Pendientes:</p>
+     <p class="data--detail"><?php  echo $this->master->contarPedidosPendientesBy(base64_decode($_GET['data']),date('Y-m-d'))['total'];?> </p>
+   </div>
+   <div class="detail">
+     <p class="item--detail">Total de visitas Canceladas:</p>
+     <p class="data--detail"><?php  echo $this->master->contarPedidosCanceladasBy(base64_decode($_GET['data']))['total'];?> </p>
+   </div>
+   <div class="detail">
+     <a href="#"  class="contact--customer" id="<?php echo $data[0]['usu_codigo']; ?>">Contactar encargado</a>
+   </div>
 
-		</ul>
-	</div>
-	<div class="wrap--btns">
-		<h2>Visitas para hoy</h2>
-		<ul>
-			<?php foreach($data as $row){?>
-					<li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
+   </ul>
+ </div>
+ <div class="wrap--btns">
+   <h2>Visitas para hoy</h2>
+   <ul>
+     <?php foreach($data as $row){?>
+         <li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
 
-				<?php } ?>
-			</ul>
-	</div>
+       <?php } ?>
+     </ul>
+ </div>
 </div>
 
 <div id="contact"></div>
 <?php }else{
-	$result = $this->master->selectBy('usuario',array('usu_codigo',base64_decode($_GET['data'])));
-	echo "<b>".$result['usu_primer_nombre']." ".$result['usu_primer_apellido']."</b> no tiene ninguna visita programada para hoy. <a href='pedidos'>Programar Visita</a>";
+ $result = $this->master->selectBy('usuario',array('usu_codigo',base64_decode($_GET['data'])));
+ echo "<b>".$result['usu_primer_nombre']." ".$result['usu_primer_apellido']."</b> no tiene ninguna visita programada para hoy. <a href='pedidos'>Programar Visita</a>";
 }
 ?>
-	<div class="orders-future">
-		<?php
-		$data_future = $this->master->verDetalleRutaFutura(date('Y-m-d'),base64_decode($_GET['data']));
-		if ($data_future!=array()) { ?>
-			<h1>Visitas Futuras</h1>
-			<?php foreach($data_future as $row){?>
-				<li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
-			<?php }
-		}else{
-			echo "<h1>No hay visitas Futuras</h1>";
-		}
-			?>
-	</div>
-	<div class="orders-pending">
-		<?php
-		$data_future = $this->master->verDetalleRutaAplazada(date('Y-m-d'),base64_decode($_GET['data']));
-		if ($data_future!=array()) { ?>
-			<h1>Visitas Pendientes</h1>
-			<?php foreach($data_future as $row){?>
-				<li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
-			<?php }
-		}else{
-			echo "<h1>No hay visitas Pendientes</h1>";
-		}
-			?>
-	</div>
-	<div class="orders-cancel">
-		<?php
-		$data_cancel = $this->master->verDetalleRutaCancelada(base64_decode($_GET['data']));
-		if ($data_cancel!=array()) { ?>
-			<h1>Visitas Canceladas</h1>
-			<?php foreach($data_cancel as $row){?>
-				<li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
-			<?php }
-		}else{
-			echo "<h1>No hay visitas Canceladas</h1>";
-		}
-			?>
-	</div>
-	<div class="orders-end">
-		<?php
-		$data_end = $this->master->verDetalleRutaFinalizadaBy(base64_decode($_GET['data']));
-		if ($data_end!=array()) { ?>
-			<h1>Visitas Terminadas</h1>
-			<?php foreach($data_end as $row){?>
-				<li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
-			<?php }
-		}else{
-			echo "<h1>No hay visitas Canceladas</h1>";
-		}
-			?>
-<<<<<<< HEAD
-	</div>
-=======
-	</div> 
->>>>>>> e78f349609bdfe5649d4bbe2f4a46b6dfe262242
+ <div class="orders-future">
+   <?php
+   $data_future = $this->master->verDetalleRutaFutura(date('Y-m-d'),base64_decode($_GET['data']));
+   if ($data_future!=array()) { ?>
+     <h1>Visitas Futuras</h1>
+     <?php foreach($data_future as $row){?>
+       <li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
+     <?php }
+   }else{
+     echo "<h1>No hay visitas Futuras</h1>";
+   }
+     ?>
+ </div>
+ <div class="orders-pending">
+   <?php
+   $data_future = $this->master->verDetalleRutaAplazada(date('Y-m-d'),base64_decode($_GET['data']));
+   if ($data_future!=array()) { ?>
+     <h1>Visitas Pendientes</h1>
+     <?php foreach($data_future as $row){?>
+       <li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
+     <?php }
+   }else{
+     echo "<h1>No hay visitas Pendientes</h1>";
+   }
+     ?>
+ </div>
+ <div class="orders-cancel">
+   <?php
+   $data_cancel = $this->master->verDetalleRutaCancelada(base64_decode($_GET['data']));
+   if ($data_cancel!=array()) { ?>
+     <h1>Visitas Canceladas</h1>
+     <?php foreach($data_cancel as $row){?>
+       <li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
+     <?php }
+   }else{
+     echo "<h1>No hay visitas Canceladas</h1>";
+   }
+     ?>
+ </div>
+ <div class="orders-end">
+   <?php
+   $data_end = $this->master->verDetalleRutaFinalizadaBy(base64_decode($_GET['data']));
+   if ($data_end!=array()) { ?>
+     <h1>Visitas Terminadas</h1>
+     <?php foreach($data_end as $row){?>
+       <li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
+     <?php }
+   }else{
+     echo "<h1>No hay visitas Canceladas</h1>";
+   }
+     ?>
+ </div>
 
-  </article>
-  </div>
-  </section>
- 	 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-   <script src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-   <script src="views/assets/js/main.js"></script>
-   <script src="views/assets/js/config.js"></script>
-   <script src="views/assets/js/orders-admin.js"></script>
-	 <script>
-		//contacto
-		$(".contact--customer").click(function(){
-			var user = this.id;
-			$.ajax({
-				url:"index.php?controller=config&a=contact",
-				type:"post",
-				dataType:"json",
-				data:({id : user}),
-				success:function(result){
-					$("#contact").html(result);
-				},
-				error:function(result) {
-					console.log(result);
-				}
-			});
-		});
+ </article>
+ </div>
+ </section>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+  <script src="views/assets/js/main.js"></script>
+  <script src="views/assets/js/config.js"></script>
+  <script src="views/assets/js/orders-admin.js"></script>
+  <script>
+   //contacto
+   $(".contact--customer").click(function(){
+     var user = this.id;
+     $.ajax({
+       url:"index.php?controller=config&a=contact",
+       type:"post",
+       dataType:"json",
+       data:({id : user}),
+       success:function(result){
+         $("#contact").html(result);
+       },
+       error:function(result) {
+         console.log(result);
+       }
+     });
+   });
 
-		//enviar Correo
-		function sendMail(){
-			var data = [];
-			$(".dataContact").each(function(){
-				data.push(this.value);
-			});
-			$.ajax({
-				url:"index.php?controller=contact&a=sendMail",
-				type:"post",
-				dataType:"json",
-				data: ({values:data}),
-				beforeSend: function() {
-						$("#sendButton").after("<div id='message-load'>Enviando</div>");
-				},
-				success:function(result){
-					console.log(result);
-				},
-				error:function(result){
-					console.log(result);
-				},
-				complete: function() {
-					$("#message-load").remove();
-					$("#contact").empty();
-			 }
-			});
-		}
+   //enviar Correo
+   function sendMail(){
+     var data = [];
+     $(".dataContact").each(function(){
+       data.push(this.value);
+     });
+     $.ajax({
+       url:"index.php?controller=contact&a=sendMail",
+       type:"post",
+       dataType:"json",
+       data: ({values:data}),
+       beforeSend: function() {
+           $("#sendButton").after("<div id='message-load'>Enviando</div>");
+       },
+       success:function(result){
+         console.log(result);
+       },
+       error:function(result){
+         console.log(result);
+       },
+       complete: function() {
+         $("#message-load").remove();
+         $("#contact").empty();
+      }
+     });
+   }
 
-		</script>
-    </body>
+   </script>
+   </body>
 </html>
 <?php }else{
-	echo "No Tienes permiso patra acceder a este  modulo";
+ echo "No Tienes permiso patra acceder a este  modulo";
 }?>
--- views/modules/admin/route/today.php
+++ views/modules/admin/route/today.php
@@ -5,18 +5,12 @@ $data = $this->master->verDetalleRuta(date('Y-m-d'),base64_decode($_GET['data'])
 $_SESSION['mapRouteToday']= $data;
 if($data!=array()){ ?>
 <div class="route">
-		<h1><?php echo $data[0]['usu_primer_nombre']." ".$data[0]['usu_primer_apellido'];?> </h1>
-		<p><?php echo $data[0]['usu_correo']?></p>
-		<p><?php echo $data[0]['usu_celular'];?></p>
-		<a href="#" class="contact--customer" id="<?php echo $data[0]['usu_codigo']; ?>"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+	<h1><?php echo $data[0]['usu_primer_nombre']." ".$data[0]['usu_primer_apellido'];?> </h1>
+	<p><?php echo $data[0]['usu_correo']?></p>
+	<p><?php echo $data[0]['usu_celular'];?></p>
 </div>
 <div class="visit today">
 	<h1>visitas para hoy <?php echo $count['total'];?></h1>
-<?php }else{
-	$result = $this->master->selectBy('usuario',array('usu_codigo',base64_decode($_GET['data'])));
-	echo "<b>".$result['usu_primer_nombre']." ".$result['usu_primer_apellido']."</b> no tiene ninguna visita programada para hoy. <a href='pedidos'>Programar Visita</a>";
-}
-?>
 	<table>
 		<tr>
 			<th>cliente</th>
@@ -24,20 +18,6 @@ if($data!=array()){ ?>
 			<th>direccion</th>
 			<th>Hora Aprox.</th>
 		</tr>
-<<<<<<< HEAD
-		<tr>
-			<td>tales s.a.s</td>
-			<td>carrera 45 aa nro. 86b-123</td>
-		</tr>
-		<tr>
-			<td>tales s.a.s</td>
-			<td>carrera 45 aa nro. 86b-123</td>
-		</tr>
-		<tr>
-			<td>tales s.a.s</td>
-			<td>carrera 45 aa nro. 86b-123</td>
-		</tr>
-=======
 		<?php
 			foreach($data as $row){
 				$user = $this->master->selectBy("usuario",array('usu_codigo',$row['usu_codigo']));
@@ -51,42 +31,9 @@ if($data!=array()){ ?>
 				</tr>
 
 		<?php } ?>
->>>>>>> e78f349609bdfe5649d4bbe2f4a46b6dfe262242
 	</table>
-</div>
-<<<<<<< HEAD
-<div class="visitMaps">
-	<p>mapa de ruta</p>
-	<div id="map"></div>
-	<div id="directions"></div>
-</div>
-<div class="visit future">
-	<div class="orders-future">
-		<?php
-		$data_future = $this->master->verDetalleRutaFutura(date('Y-m-d'),base64_decode($_GET['data']));
-		if ($data_future!=array()) { ?>
-			<h1> Futuras visitas</h1>
-			<?php foreach($data_future as $row){?>
-				<table>
-					<tr>
-						<th>codido</th>
-						<th>cliente</th>
-						<th>direccion</th>
-					</tr>
-					<tr>
-						<td><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></td>
-						<td><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></td>
-						<td><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></td>
-					</tr>
-				</table>
 
-			<?php }
-		}else{
-			echo "<h1>No hay visitas Futuras</h1>";
-		}
-			?>
-	</div>
-=======
+</div>
 <!-- <a href="#"  class="contact--customer" id="<?php echo $data[0]['usu_codigo']; ?>">Contactar encargado</a> -->
 
 <!-- contacto -->
@@ -108,39 +55,23 @@ if($data!=array()){ ?>
 		echo "<h1>No hay visitas Futuras</h1>";
 	}
 		?>
->>>>>>> e78f349609bdfe5649d4bbe2f4a46b6dfe262242
 </div>
-<div class="visit postpone">
-	<div class="orders-pending">
-		<?php
-		$data_future = $this->master->verDetalleRutaAplazada(date('Y-m-d'),base64_decode($_GET['data']));
-		if ($data_future!=array()) { ?>
-			<h1>Visitas Pendientes</h1>
-			<table>
-				<tr>
-					<th>codigo</th>
-					<th>cliente</th>
-					<th>direccion</th>
-				</tr>
-					<?php foreach($data_future as $row){?>
-						<tr>
-						<td><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></td>
-						<td><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></td>
-						<td><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></td>
-					</tr>
-					<?php }?>
-			</table>
-		<?php }else{
-			echo "<h1>No hay visitas Pendientes</h1>";
-		}
-			?>
-	</div>
+<div class="orders-pending">
+	<?php
+	$data_future = $this->master->verDetalleRutaAplazada(date('Y-m-d'),base64_decode($_GET['data']));
+	if ($data_future!=array()) { ?>
+		<h1>Visitas Pendientes</h1>
+		<?php foreach($data_future as $row){?>
+			<li class="opcins--order"><a href="ver-pedido-<?php echo rtrim(strtr(base64_encode($row['ped_token']), '+/', '-_'), '=');?>" target="_blank"><?php echo $row['ped_token']?></a></li>
+		<?php }
+	}else{
+		echo "<h1>No hay visitas Pendientes</h1>";
+	}
+		?>
 </div>
-<!-- contacto -->
-			<div id="contact"></div>
-    </article>
-    </div>
-    </section>
+                </article>
+        	</div>
+        </section>
  	 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
    <script src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
