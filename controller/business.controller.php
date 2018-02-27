e8f4cec4b5615d883d61be68cd7f01cf8579d589
CristianAlexis10 <cristian1020011@gmail.com>
2018-02-27 14:56:00 -0500

    a

--- controller/business.controller.php
+++ controller/business.controller.php
@@ -19,9 +19,13 @@
 			require_once "views/include/scope.footer.php";
 		}
 		function profile(){
+		if (isset($_SESSION['CUSTOMER']['ROL']) && !isset($_SESSION['CUSTOMER']['CLIENT'])) {
 			require_once "views/include/scope.header.php";
 			require_once "views/modules/admin/profile/business/index.php";
 			require_once "views/include/scope.footer.php";
+		}else{
+			header("Location:maxirecargas");
+		}
 		}
 		function readByNit(){
 			$data=$_POST['data'];
