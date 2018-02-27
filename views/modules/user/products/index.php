d96584fa128e430f1934efe639d4542ade05c570
evelinFrontend <ginatha97@gmail.com>
2018-02-27 15:10:35 -0500

    pagination

--- views/modules/user/products/index.php
+++ views/modules/user/products/index.php
@@ -1,42 +1,37 @@
-
-  <div class="banner">
-    <h1 class="title_banner">categorias</h1>
-  </div>
-  <div class="banneritem Btoner">
-    <span><i class="fa fa-arrow-left" aria-hidden="true" id="pedAtras"></i></span>
-    <h1 class="titleitem_banner" id="categoryName">toner</h1>
-    <div class="container_search">
-      <div class="search_input">
-        <input type="search" id="readBy" placeholder="buscar por referencia,marca">
-        <button type="button" name="button"  id="buscar" ><i class="fa fa-search" aria-hidden="true"></i>
-        </button>
-      </div>
-
+    <div class="banner">
+      <h1 class="title_banner">categorias</h1>
     </div>
-  </div>
+    <div class="banneritem Btoner">
+      <span><i class="fa fa-arrow-left" aria-hidden="true" id="pedAtras"></i></span>
+      <h1 class="titleitem_banner" id="categoryName">toner</h1>
+      <div class="container_search">
+        <div class="search_input">
+          <input type="search" id="readBy" placeholder="buscar por referencia,marca">
+          <button type="button" name="button"  id="buscar" ><i class="fa fa-search" aria-hidden="true"></i>
+          </button>
+        </div>
 
-  <div class="container--products">
-    <div class="products">
-      <div class="item toner Btncategoria" id="Toner">
-        <img src="views/assets/image/toner.png">
-        <h3 >toner</h3>
-      </div>
-      <div class="item cartucho Btncategoria" id="Cartuchos">
-        <img src="views/assets/image/cartuchos.png" >
-        <h3>cartuchos</h3>
-      </div>
-      <div class="item impresora Btncategoria" id="Impresoras">
-        <img src="views/assets/image/impresora.png">
-        <h3>impresoras</h3>
-      </div>
-      <div class="item computadores Btncategoria" id="Computador">
-        <img src="views/assets/image/computador.png">
-        <h3>computadores</h3>
       </div>
     </div>
-    <span class="menuProduct"><i class="fa fa-bars" aria-hidden="true" id="menu"></i></span>
-  </div>
-<div class="supercontainer">
-  <div class="container--grid"></div>
-
-</div>
+    <div class="container--products">
+      <div class="products">
+        <div class="item toner Btncategoria" id="Toner">
+          <img src="views/assets/image/toner.png">
+          <h3 >toner</h3>
+        </div>
+        <div class="item cartucho Btncategoria" id="Cartuchos">
+          <img src="views/assets/image/cartuchos.png" >
+          <h3>cartuchos</h3>
+        </div>
+        <div class="item impresora Btncategoria" id="Impresoras">
+          <img src="views/assets/image/impresora.png">
+          <h3>impresoras</h3>
+        </div>
+        <div class="item computadores Btncategoria" id="Computador">
+          <img src="views/assets/image/computador.png">
+          <h3>computadores</h3>
+        </div>
+      </div>
+      <span class="menuProduct"><i class="fa fa-bars" aria-hidden="true" id="menu"></i></span>
+    </div>  
+    <div class="container--grid"></div>
