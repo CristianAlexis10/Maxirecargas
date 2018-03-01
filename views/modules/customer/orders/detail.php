cd23a8af6072a829e066da5f45c83cb53f8a4c96
evelinFrontend <ginatha97@gmail.com>
2018-03-01 17:37:19 -0500

    detalles de productos

--- views/modules/customer/orders/detail.php
+++ views/modules/customer/orders/detail.php
@@ -5,72 +5,74 @@
   	}
   </style>
   <?php
-
    $_SESSION['ped_detail_token'] = $data_order[0]['ped_token'];
   ?>
-  <div class="mudules orders detail" id="detail-reload">
-  	<div class="wrap--info">
-  		<div class="detail">
-  			<p class="item--detail">Codigo del pedido:</p>
-  			<p class="data--detail"><?php echo $data_order[0]['ped_token'];?> </p>
-  		</div>
-  		<div class="detail">
-  			<p class="item--detail">Cliente:</p>
-  			<p class="data--detail"><?php echo $data_order[0]['usu_primer_nombre']." ".$data_order[0]['usu_primer_apellido'];?> </p>
-  		</div>
-  		<div class="detail">
-  			<p class="item--detail">Fecha de realización:</p>
-  			<p class="data--detail"><?php echo $data_order[0]['ped_fecha'];?> </p>
-  		</div>
-  		<div class="detail">
-  			<p class="item--detail">Dirección:</p>
-  			<p class="data--detail"><?php echo $data_order[0]['ciu_nombre'].", ".$data_order[0]['ped_direccion'];?>  </p>
-  		</div>
-  		<div class="detail">
-  			<p class="item--detail">Fecha  de entrega:</p>
-  			<p class="data--detail"><?php echo $data_order[0]['ped_fecha_entrega'];?> </p>
-  		</div>
-  		<div class="detail">
-  			<p class="item--detail">Hora Aproximada  de entrega:</p>
-  			<p class="data--detail"><?php echo $data_order[0]['ped_hora_entrega'];?> </p>
-  		</div>
-  		<?php if ($data_order[0]['ped_encargado']!=null) {
-  			$data_employe = $this->master->selectBy('usuario',array('usu_codigo',$data_order[0]['ped_encargado']))
-  			?>
-  				<div class="detail">
-  					<p class="item--detail">Encargado:</p>
-  					<p class="data--detail"><?php echo $data_employe['usu_primer_nombre']." ".$data_employe['usu_primer_apellido'] ?> </p>
-  				</div>
-  		<?php } ?>
-  		<div class="detail">
-  			<p class="item--detail">Estado:</p>
-  			<p class="data--detail"><?php echo $data_order[0]['ped_estado'];?> </p>
-  		</div>
-  			<li class="item mapa">
-  				 <div id="map"></div>
-  			 </li>
-  		</ul>
-  	</div>
-  	<div class="wrap--btns">
-  		<ul>
-  			<li class="opcins--order"><a href="#" id="viewAllProducts">ver articulos</a></li>
-  			<?php
-  				if ($data_order[0]['ped_encargado']!=null) {?>
+
+  <div class="container--detail">
+    <div class="content--detail">
+      <h1>Detalles De Pedidos</h1>
+         <div class="detail">
+           <p class="item--detail">Codigo del pedido:</p>
+           <p class="data--detail"><?php echo $data_order[0]['ped_token'];?> </p>
+         </div>
+         <div class="detail">
+           <p class="item--detail">Cliente:</p>
+           <p class="data--detail"><?php echo $data_order[0]['usu_primer_nombre']." ".$data_order[0]['usu_primer_apellido'];?> </p>
+         </div>
+         <div class="detail">
+           <p class="item--detail">Fecha de realización:</p>
+           <p class="data--detail"><?php echo $data_order[0]['ped_fecha'];?> </p>
+          </div>
+         <div class="detail">
+           <p class="item--detail">Dirección:</p>
+           <p class="data--detail"><?php echo $data_order[0]['ciu_nombre'].", ".$data_order[0]['ped_direccion'];?>  </p>
+         </div>
+         <div class="detail">
+           <p class="item--detail">Fecha  de entrega:</p>
+           <p class="data--detail"><?php echo $data_order[0]['ped_fecha_entrega'];?> </p>
+         </div>
+         <div class="detail">
+           <p class="item--detail">Hora Aproximada  de entrega:</p>
+           <p class="data--detail"><?php echo $data_order[0]['ped_hora_entrega'];?> </p>
+         </div>
+         <?php if ($data_order[0]['ped_encargado']!=null) {
+           $data_employe = $this->master->selectBy('usuario',array('usu_codigo',$data_order[0]['ped_encargado']))
+           ?>
+             <div class="detail">
+               <p class="item--detail">Encargado:</p>
+               <p class="data--detail"><?php echo $data_employe['usu_primer_nombre']." ".$data_employe['usu_primer_apellido'] ?> </p>
+             </div>
+         <?php } ?>
+         <div class="detail">
+           <p class="item--detail">Estado:</p>
+           <p class="data--detail"><?php echo $data_order[0]['ped_estado'];?> </p>
+         </div>
+         <div class="wrap--btns">
+         <ul>
+           <li class="opcins--order"><a href="#" id="viewAllProducts">ver articulos</a></li>
+           <?php
+             if ($data_order[0]['ped_encargado']!=null) {?>
             <li class="opcins--order"><a href="#"  class="contact--customer" id="<?php echo $data_order[0]['ped_encargado']; ?>">Contactar encargado</a></li>
-  					<?php } ?>
-
-  		</ul>
-  	</div>
+           <?php } ?>
+         </ul>
+       </div>
+     </div>
+     <div class="content--detailRight">
+       <img src="views/assets/image/flat/check.png">
+       <h2>Estamos feliz de trabajar para usted</h2>
+       <h1>¡Gracias por trabajar con nosotros!</h1>
+     </div>
+    <div class="detail--figure">
+    </div>
   </div>
-
 
 
   <!-- contacto -->
   <div id="contact"></div>
 
-                  </article>
-          	</div>
-          </section>
+  </article>
+    </div>
+    </section>
    	 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
      <script src="views/assets/js/orders-admin.js"></script>
