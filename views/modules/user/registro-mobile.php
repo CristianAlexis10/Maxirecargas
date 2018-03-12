04bde42a324a2912b03e5397bc6dec96b1338be1
evelinFrontend <ginatha97@gmail.com>
2018-03-12 14:10:18 -0500

    hace el htacc

---
+++ views/modules/user/registro-mobile.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title> Maxirecargas</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="views/assets/image/icn-maxi.png">
+    <link rel="stylesheet" href="views/assets/css/main-user.css">
+    <link rel="stylesheet" href="views/assets/css/animate.css">
+    <link rel="stylesheet" href="views/assets/css/responsive.css">
+  </head>
+  <body>
+    <div class="registrar--mobile">
+      <h1>Crea tu cuenta</h1>
+      <p>Registrate y conoce los beneficios que tenemos para ti</p>
+    </div>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <!-- <script type="text/javascript" src="views/assets/js/main-user.js"></script>
+    <script type="text/javascript" src="views/assets/js/contact.js"></script>
+    <script type="text/javascript" src="views/assets/js/menu.js"></script>
+   -->
+  </body>
+  </html>
