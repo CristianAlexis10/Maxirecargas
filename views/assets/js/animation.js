3db46e5100e6eee5d7fbf8aa8201e002c3acc91f
evelinFrontend <ginatha97@gmail.com>
2018-03-15 15:07:42 -0500

    animated

--- views/assets/js/animation.js
+++ views/assets/js/animation.js
@@ -6,7 +6,6 @@ var ourScene =  new ScrollMagic.Scene({
  // offset : 10,
 })
 .setClassToggle('.section--one','zoomIn')
-.addIndicators()
 .addTo(controller);
 
 var controller =  new ScrollMagic.Controller();
@@ -17,7 +16,7 @@ var ourScene =  new ScrollMagic.Scene({
  // duration: 300
 })
 .setClassToggle('.two--text','slideInLeft')
-.addIndicators()
+
 .addTo(controller);
 
 var controller =  new ScrollMagic.Controller();
@@ -28,29 +27,48 @@ var ourScene =  new ScrollMagic.Scene({
  offset : 300
 })
 .setClassToggle('.two--text2','slideInLeft')
-.addIndicators()
+
 .addTo(controller);
 
 var controller =  new ScrollMagic.Controller();
 var ourScene =  new ScrollMagic.Scene({
- triggerElement: '.three',
- triggerHook: 0.7,
+ triggerElement: '.three--rigth',
+ triggerHook: 0.5,
  reverse : true,
  offset : 100
 })
-.setClassToggle('.imgAnimation','rotateInDownLeft')
-.addIndicators()
+.setClassToggle('.imgAnimation','animateImgs')
+
 .addTo(controller);
 
 var controller =  new ScrollMagic.Controller();
 var ourScene5 =  new ScrollMagic.Scene({
+ triggerElement: '.three--rigth',
+ triggerHook: 0.5,
+ reverse : true,
+ offset : 200
+})
+.setClassToggle('.imgAnimationServ','animateImgs')
+
+.addTo(controller);
+var controller =  new ScrollMagic.Controller();
+var ourScene8 =  new ScrollMagic.Scene({
+ triggerElement: '.three',
+ triggerHook: 0.7,
+ reverse : true,
+})
+.setClassToggle('.threeTitle','slideInLeft')
+
+.addTo(controller);
+var controller =  new ScrollMagic.Controller();
+var ourScene9 =  new ScrollMagic.Scene({
  triggerElement: '.three',
  triggerHook: 0.7,
  reverse : true,
  offset : 200
 })
-.setClassToggle('#services-servicio','jackInTheBox')
-.addIndicators()
+.setClassToggle('.threeItem','slideInLeft')
+
 .addTo(controller);
 
 var controller =  new ScrollMagic.Controller();
@@ -61,7 +79,7 @@ var ourScene6 =  new ScrollMagic.Scene({
  offset : 50
 })
 .setClassToggle('.title','slideInLeft')
-.addIndicators()
+
 .addTo(controller);
 var controller =  new ScrollMagic.Controller();
 var ourScene7 =  new ScrollMagic.Scene({
@@ -71,5 +89,15 @@ var ourScene7 =  new ScrollMagic.Scene({
  offset : 100
 })
 .setClassToggle('.img--ambiente','hambiente')
-.addIndicators()
+
+.addTo(controller);
+var controller =  new ScrollMagic.Controller();
+var ourScene7 =  new ScrollMagic.Scene({
+ triggerElement: '.four--contact',
+ triggerHook: 0.7,
+ reverse : true,
+ offset : 50
+})
+.setClassToggle('.four--contact','slideInLeft')
+
 .addTo(controller);
