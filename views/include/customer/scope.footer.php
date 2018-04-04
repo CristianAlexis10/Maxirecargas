24b2758b838e51df413742eb2a850ce5877a8136
evelinFrontend <ginatha97@gmail.com>
2018-04-04 00:06:44 -0500

    include

---
+++ views/include/customer/scope.footer.php
@@ -0,0 +1,8 @@
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script type="text/javascript" src="views/assets/js/copepr.js"></script>
+    <script type="text/javascript" src="views/assets/js/main-user.js"></script>
+    <script type="text/javascript" src="views/assets/js/orders.js"></script>
+    <script type="text/javascript" src="views/assets/js/menu.js"></script>
+  </body>
+</html>
