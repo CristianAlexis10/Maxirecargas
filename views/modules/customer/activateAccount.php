8f661356441a244596ecee14462dda915b662abe
evelinFrontend <ginatha97@gmail.com>
2018-04-03 22:55:36 -0500

    listo activar cuenta

--- views/modules/customer/activateAccount.php
+++ views/modules/customer/activateAccount.php
@@ -2,18 +2,17 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Último paso</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <link rel="shortcut icon" href="views/assets/image/icn-maxi.png">
-    <link rel="stylesheet" href="views/assets/css/main-user.css">
-    <link rel="stylesheet" href="views/assets/css/responsive.css">
+    <title>Ultimo paso</title>
+    <link rel="shortcut icon" href="views/assets/image/logo.png">
+    <link rel="stylesheet" href="views/assets/css/error.css">
   </head>
   <body>
-    <div class="container--activar">
-      <div class="content--activar">
-        <img src="views/assets/image/logo.png">
-        <h1>Último paso</h1>
-        <a href="activarCuenta-<?php echo $_GET['token'] ?>">Click aquí para activar tu cuenta</a>
+    <div class="container--error">
+      <img src="views/assets/image/logo.png">
+      <div class="error">
+        <h2>Último paso</h2>
+        <a class="hipervinculo" href="activarCuenta-<?php echo $_GET['token'] ?>">Click aquí para activar tu cuenta</a>
       </div>
       <div class="figura1"></div>
       <div class="figura2"></div>
