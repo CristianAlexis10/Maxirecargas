dd4c44cf1d288943ef1612335b13bf6aa3677d39
CristianAlexis10 <cristian1020011@gmail.com>
2018-04-04 21:54:13 -0500

    hg

--- views/include/scope.profile.php
+++ views/include/scope.profile.php
@@ -1,7 +1,7 @@
 
 <i class="fa fa-arrow-left" aria-hidden="true" id="arrowLeft"></i>
         <div class="userProfile--img">
-            <a href="perfil-empresarial">
+            <!-- <a href="perfil-empresarial"> -->
               <img src="views/assets/image/logo.png" alt="">
-            </a>
+            <!-- </a> -->
           </div>
--- views/modules/admin/profile/settings.php
+++ views/modules/admin/profile/settings.php
@@ -7,6 +7,7 @@
     <li><a href="#config-permisos">Permisos</a></li>
     <li><a href="#general">General</a></li>
     <li><a href="#contacto">Datos de contacto</a></li>
+    <li><a href="#maxi">Acerca de Maxirecargas</a></li>
   </ul>
   <div id="config-permisos">
       <form id="frmNewRol">
@@ -204,5 +205,59 @@
             <input type="submit" id="btn-coso" class="btn">
           </div>
         </form>
+      </div>
+      <div id="maxi">
+        <?php
+        $data = $this->master->selectAll("gestion_web");
+        ?>
+        <div class="modules profile">
+          <div class="title">
+            <p>MAXIRECARGAS</p>
+          </div>
+          <div class="maxContainer_profile">
+            <?php if($_SESSION['CUSTOMER']['ROL']==2){?>
+            <div class="frmprofile">
+              <form id="frmProfileBusi">
+                  <div class="form-group">
+                    <label for="micro_des" class="select label">Micro Descripción:</label>
+                    <textarea class="input dataUptadeBusi textarea" id="micro_des"   required><?php echo $data[0]['gw_micro_des']?></textarea>
+                  </div>
+                  <div class="form-group">
+                    <label for="mision" class="select label">Misión:</label>
+                    <textarea class="input dataUptadeBusi textarea" id="mision" required><?php echo $data[0]['gw_mision']?></textarea>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="vision" class="select label">Visión:</label>
+                    <textarea class="input dataUptadeBusi textarea" id="vision"   required><?php echo $data[0]['gw_vision']?></textarea>
+                  </div>
+                  <div class="form-group">
+                    <label for="pma" class="select label">Politica de medio ambiente:</label>
+                    <textarea class="input dataUptadeBusi textarea" id="pma" required><?php echo $data[0]['gw_politicas']?></textarea>
+                  </div>
+
+                <div class="wrap_two_formgroup">
+                    <div class="form-group">
+                    <!-- <a href="configuraciones#contacto">Actualizar datos de contacto</a> -->
+                    </div>
+
+              </div>
+              <div class="form-group">
+                  <button type="submit" class="btn">hacer cambios</button>
+              </div>
+            </div>
+          <?php }else{?>
+                <h1>Misión</h1>
+                <p><?php echo $data[0]['gw_mision']?></p>
+                <h1>Visión</h1>
+                <p><?php echo $data[0]['gw_vision']?></p>
+                <h1>Politica de medio ambiente</h1>
+                <p><?php echo $data[0]['gw_politicas']?></p>
+          <?php  } ?>
+          </div>
+        </form>
+
+        </div>
+
       </div>
 </div>
