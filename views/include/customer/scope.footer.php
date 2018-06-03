1c6023a745136381a53342dbef36ad670b837154
CristianAlexis10 <cristian1020011@gmail.com>
2018-06-02 23:05:00 -0500

    pinche evelin

--- views/include/customer/scope.footer.php
+++ views/include/customer/scope.footer.php
@@ -1,9 +1,12 @@
     <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
+    <script src="views/assets/js/croppie.js"></script>
     <script type="text/javascript" src="views/assets/js/copepr.js"></script>
     <script type="text/javascript" src="views/assets/js/main-user.js"></script>
     <script type="text/javascript" src="views/assets/js/orders.js"></script>
     <script type="text/javascript" src="views/assets/js/menu.js"></script>
+    <script type="text/javascript" src="views/assets/js/cropp-profile.js"></script>
     <script src="views/assets/lib/shortcut/shortcut.js"> </script>
     <script type="text/javascript" src="views/assets/js/security.js"></script>
   </body>
